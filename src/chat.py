bbe9a731818a636a2deb3c8a147cd2ae392e9163
Nathan G. Santos <nathangar01@gmail.com>
2026-05-13 20:26:09 -0300

    fix(chat): print response and rename chain variable

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,14 @@
 from search import search_prompt
 
 def main():
-    question = input("Faça sua pergunta:")
-    chain = search_prompt(question)
+    question = input("Faça sua pergunta: ")
+    response = search_prompt(question)
 
-    if not chain:
+    if not response:
         print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
         return
     
+    print(f"RESPOSTA: {response}")
     pass
 
 if __name__ == "__main__":
